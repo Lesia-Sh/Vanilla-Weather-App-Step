63f0670427f8e82747dbbd3965a175a0b5b5c072
Lesia-Sh <shvch.lv@gmail.com>
2022-07-18 18:40:14 -0400

    removed consol.log

--- src/12.js
+++ src/12.js
@@ -1,10 +1,25 @@
-function formatDay(timestamp) {
+function formatDate(timestamp) {
   let date = new Date(timestamp);
-  let hours = date.get.Hours();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = date.getMinutes();
-  let day = date.getDay();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
   //   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  return `${day} ${hours}: ${minutes}`;
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
